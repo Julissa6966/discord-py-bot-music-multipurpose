902135c6b4ce7481882d5b8fbdb2e12ae776e3f4
Julissa6966 <95084721+Julissa6966@users.noreply.github.com>
2021-11-26 14:08:48 +0000

    Create skip.py

---
+++ commands/🎶 Music/skip.py
@@ -0,0 +1,45 @@
+import discord
+import os
+import ytdl
+import discord.utils
+from discord.ext import commands
+from os import "/commands/🎶 Music/play.py",
+from ytdl import ytdlUtils, ytdlSkips, ytdlManageSongs
+from discord.utils import voicechannels
+
+command_export = [
+  "name": "skip",
+  "aliases": "Skip, SkiP, skip",
+  "description": "Skips Current Song",
+  "usage": "?skip",
+  "permissions": [],
+  "online": True,
+]
+
+bot = commands.Bot(set_prefix="?");
+bot.ytdl.skips && bot.utils();
+
+@bot.command()
+async def skip(ctx, ytdlSkipSong, *, votes=Votes):
+  if vc = contains({overNum={1}}(==![users])):
+    await ctx.send("We Have To Vote In Order To Skip This Current Song")
+    await ctx.addreactions(👍;❌);
+    if 👍 = highest.vote(self.skip.song=True)/ytdl.Skip=True(//current.song//)
+    else {
+      if ❌ = highest.vote(self.deny.skip=True)
+    }
+
+    def skip(self.skip, discordUtils):
+      await ytdl.skip.song(current):
+        if vote = 👍 = [highest.vote]
+        else if vote = ❌ = [highest.vote]=>self.deny.skip(utils.destroy.skip)
+
+    if vc = 1(user)==autoSkipSong=True()/no_Vote=True();
+    
+    else {
+      if skipped = [
+        embed = discord.embed = [
+          title = discord.Embed.title("Skipped Song")
+        ]
+      ]
+    }
