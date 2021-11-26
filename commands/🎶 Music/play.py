c81d70b0789a558f8737f25e92d3d33277b0e245
Julissa6966 <95084721+Julissa6966@users.noreply.github.com>
2021-11-26 13:55:53 +0000

    Update play.py

--- commands/🎶 Music/play.py
+++ commands/🎶 Music/play.py
@@ -1,1 +1,67 @@
+import discord
+import ytdl
+import music
+import functions
+import objects
+import on
+import Spotify, YouTube, SoundCloud
+from Spotify, YouTube, SoundCloud import API
+from discord.ext import commands
+from ytdl import ytdlSearches, ytdlOs, ytdlPlay, ytdlVolume
 
+bot = commands.Bot(set_prefix="?")
+bot.join.voiceChannel(oncommand=True)
+
+command_export = [
+  "name": "play",
+  "description": "plays the song you request",
+  "permissions": [],
+  "usage", "?play <song_name>",
+]
+
+class find_song(self, ctx.song):
+  found()
+  notfound()
+  if user === requests(song {
+    self.ytdl.search.platforms = [
+      list = {
+        "Spotify", url="https://spotify.com/song_search=?{song.name}
+        "SoundCloud", url="https://soundcloud.com/song_name=?refName={song.name}
+        "YouTube", url="https://youtube.com/{song.name}
+        allow.urls()
+        if url = self.change.field = {song.name} = {song.url}
+      }
+      if notfound = self.error:
+        await ctx.send("Error, The Song Was Not Found")
+        else {
+        if found === ytdl(play.song=True)==self.fetch.song(name=duration=artist/singer/rapper)
+        self.join(voice_channel)
+        }
+    ]
+})
+
+@bot.command(aliases=["Play, PlaY, play, PLAY, pLaY, PlAy"])
+async def play(ctx, ytdl,*, song=song):
+  for playing_song = {
+    if user === song.failed.search:
+      await error()
+      await ctx.send(\{ctx.author.mention}/, The Song Was Not Found)
+  }
+
+  def volume(ctx, ytdl.Vol):
+    set(default_volume = ctx.voice.channel="100".spikeAudio=True)/lag=None()
+  
+  else {
+    if song = found():
+      self.await.song_play = True()
+      embed = discord.Embed = {
+        title = discord.Embed.title(f"Playing {\song.name.fetch.ctx(song)\}")
+        description = discord.Embed.description(f"Song Information")
+        newField = discord.Embed.AddField(name="Song Duration", value=f"{\self.collect.{}.fetch{}ctx.song.duration\}")
+        newField = discord.Embed.AddField(name="Signer/Artist", value=f"{\self.collect {} ctx.song.artist.name}")
+        color = discord.Embed.color("RANDOM".ctx.embed.color(set(random))=True)
+        timestamp = discord.Embed.timestamp(f"{ctx.message.created_at}")
+        footer = discord.Embed.footer(text="Music 🎶")
+        author = discord.Embed.SetAuthor(name="Rover Music", icon_url=ctx.guild.icon{}.serverIcon)
+      }
+  }
